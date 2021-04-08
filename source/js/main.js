b5ceca91aa58797270b25c5dbbf29c6de349f62f
ikave <shiftgear@mail.ru>
2021-04-08 18:25:53 +0300

    Добавляет скрипты для аккардеона

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,20 @@
 'use strict';
 
 (function(){
+  const accordionButtons = document.querySelectorAll('.accordion');
 
+  accordionButtons.forEach((button) => {
+    button.addEventListener('click', function () {
+      const panel = this.nextElementSibling;
+      if (this.classList.contains('accordion--opened')) {
+        this.classList.remove('accordion--opened');
+        this.classList.add('accordion--closed');
+        panel.classList.remove('page-footer__panel--opened');
+      } else {
+        this.classList.remove('accordion--closed');
+        this.classList.add('accordion--opened');
+        panel.classList.add('page-footer__panel--opened');
+      }
+    });
+  });
 })();
