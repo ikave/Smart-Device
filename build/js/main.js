e37d16a3c28c8d69a8a3c08e69ef56e02d461524
ikave <shiftgear@mail.ru>
2021-04-13 14:12:55 +0300

    Добавляет маску для телефона, сохраняет значения полей в localStorage

--- build/js/main.js
+++ build/js/main.js
@@ -2,6 +2,88 @@
 
 (function(){
   const anchors = document.querySelectorAll('a.scroll');
+  const accordionButtons = document.querySelectorAll('.accordion');
+  const popupButton = document.querySelector('.page-header__button');
+  const popup = document.querySelector('.popup');
+  const popupForm = popup.querySelector('form');
+  const popupName = popup.querySelector('#feedback-popup-name');
+  const popupPhone = popup.querySelector('#feedback-popup-phone');
+  const popupText = popup.querySelector('#feedback-popup-text');
+  const popupButtonClose = popup.querySelector('.popup__close');
+  const phoneInputs = document.querySelectorAll('input[type="tel"]');
+  let isStorageSupport = true;
+
+  let storageName = '';
+  let storagePhone = '';
+  let storageText = '';
+
+  try {
+    storageName = localStorage.getItem('name');
+    storagePhone = localStorage.getItem('phone');
+    storageText = localStorage.getItem('text');
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  for (let input of phoneInputs) {
+    const mask = IMask(input, {
+      mask: `+{7}(000)000-00-00`
+    })
+  }
+
+  const openPopup = () => {
+    document.body.style.overflowY = 'hidden';
+    popup.classList.add('popup--opened');
+
+    if (popupName) {
+      popupName.focus();
+      popupName.value = storageName;
+    }
+
+    if (popupPhone) {
+      popupPhone.value = storagePhone;
+    }
+
+    if (popupText) {
+      popupText.value = storageText;
+    }
+
+    popup.addEventListener('click', onPopupClick);
+    window.addEventListener('keydown', onEscPress);
+    popupForm.addEventListener('submit', setLocalStorageValues);
+  };
+
+  const closePopup = function ()  {
+    document.body.style.overflowY = 'visible';
+    popup.classList.remove('popup--opened');
+
+    popup.removeEventListener('click', onPopupClick);
+    window.removeEventListener('keydown', onEscPress);
+  };
+
+  const onPopupClick = function (evt) {
+    const target = evt.target;
+    if (target.classList.contains('popup') || target === popupButtonClose) {
+      closePopup();
+    }
+  };
+
+  const onEscPress = (evt) => {
+    if (evt.key === 'Escape') {
+      evt.preventDefault();
+      closePopup();
+    }
+  }
+
+  const setLocalStorageValues = () => {
+    if (isStorageSupport) {
+      localStorage.setItem('name', popupName.value);
+      localStorage.setItem('phone', popupPhone.value);
+      localStorage.setItem('text', popupText.value);
+    }
+  }
+
+
 
   for (let anchor of anchors) {
     anchor.addEventListener('click', (evt) => {
@@ -13,8 +95,6 @@
       })
     })
   };
-
-  const accordionButtons = document.querySelectorAll('.accordion');
 
   accordionButtons.forEach((button) => {
     button.addEventListener('click', function () {
@@ -30,4 +110,6 @@
       }
     });
   });
+
+  popupButton.addEventListener('click', openPopup);
 })();
